7c1701ebfafcce3913a69c1879c4b9f03bc4ce87
ShavaughnAD <shavaughnd1@gmail.com>
2020-01-06 15:53:11 -0500

    Uploading Warlords Unity Proj
    Uploading Warlords Unity Proj

---
+++ WARLORDS/Assets/CardDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Cards cards;
+    public Image cardRarity;
+    public Image cardImage;
+    public Text cardName;
+    public Text cardDescritpion;
+    public Text cardAttack;
+    public Text cardDefense;
+
+    public void DisplayCard(Cards cardToDisplay)
+    {
+        cards = cardToDisplay;
+        cardRarity.sprite = cards.rarity;
+        cardImage.sprite = cards.cardSprite;
+        cardName.text = cards.cardName;
+        cardDescritpion.text = cards.cardText;
+        cardAttack.text = cards.cardAttack.ToString();
+        cardDefense.text = cards.cardDefense.ToString();
+    }
+}
---
+++ WARLORDS/Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float startTime = 30;
+    float currTime = 0;
+
+    void Start()
+    {
+        timerText = GetComponentInChildren<Text>();
+        currTime = startTime;
+    }
+
+    void Update()
+    {
+        timerText.text = currTime.ToString("F0");
+        currTime -= (1 * Time.deltaTime);
+        if(currTime <= 0)
+        {
+            //TODO Change Turn
+        }
+    }
+}
